287bdf7a085c6f4df6d4f6356bbbd692bce1bf75
FilVincent <filanivincent001@gmail.com>
2024-05-05 22:22:28 +0100

    function to sort a string list

---
+++ sort-strings-by-length.py
@@ -0,0 +1,9 @@
+
+def sort_list(list_):
+  sorted_list = sorted(list_, key=len)
+  return sorted_list
+
+#sorted_list = sort_list(list_)
+list_ =['filani','olasunkanmi','vincent','kolawole','king']
+
+print(sort_list(list_))
